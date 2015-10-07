2aea5e60a5c839520475a280f760f2ae8934e030
Michel Estermann <estermann.michel@gmail.com>
2015-10-08 00:09:20 +0200

    Updated linux build script.

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
 WORKSPACE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${WORKSPACE_DIR}
-make prepare
-make clean
-make unittests
-make specs
-make igloo-tests
-make features
-make doc
+cmake --build build --target external_dependencies "$@"
+cmake --build build --target all "$@"
 
